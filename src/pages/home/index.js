775b4edc49182af0b7c0de8f3782d4408d85a518
hishmatrai <hishmatrai.2018@gmail.com>
2023-02-14 19:50:33 +0500

    update home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,22 +1,59 @@
 import React, { useState } from "react";
+import "./index.css";
 const Home = () => {
-  const [showHeading, setShowHeading] = useState();
-//   if()
+  const [formType, setFormType] = useState("Physical");
+  const [fullName, setFullName] = useState("");
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("");
+  const SignUp = () => {
+    if (fullName === "") {
+      setMessage("full name");
+      setMessageType("error");
+    } else {
+      setMessage("full name .......");
+      setMessageType("success");
+      const userData = {
+        fullName:fullName,
+        formType:formType
+      }
+      console.log("userData",userData);
+      setFullName("")
+    }
+  };
   return (
     <div>
-      {/* {showHeading &&} */}
-      {/* {showHeading ?} */}
-      {showHeading && <h1>Home Page</h1>}
-      <button style={{backgroundColor:showHeading? "red":"blue",color:!showHeading && "white"}} onClick={() => setShowHeading(!showHeading)}>
-        {showHeading ? "Hide" : "Show"}
+      <button
+        className={formType === "Physical" ? "selected-button" : "button"}
+        onClick={() => setFormType("Physical")}
+      >
+        Physical
       </button>
-
-      {/* {showHeading ? (
-        <button onClick={() => setShowHeading(false)}>Hide</button>
-      ) : (
-        <button onClick={() => setShowHeading(true)}>Show</button>
-      )} */}
+      <button
+        className={formType === "Online" ? "selected-button" : "button"}
+        onClick={() => setFormType("Online")}
+      >
+        Online
+      </button>
+      <p>Apply form here for {formType} classes.</p>
+      <input
+        type="text"
+        placeholder="full name"
+        value={fullName}
+        onChange={(fullName) => setFullName(fullName.target.value)}
+      />
+      <p style={{ color: messageType === "error" ? "red" : "green" }}>
+        {message}
+      </p>
+      <button onClick={SignUp}>submit</button>
     </div>
   );
 };
 export default Home;
+
+
+
+
+
+
+
+// Login, Sign Up ,Update, Edit, Delete ,Log Out ,Vefify 
