39b5d8eea84c4626adf8f299745ca960e616a885
hishmatrai <hishmatrai.2018@gmail.com>
2023-02-17 19:51:14 +0500

    update

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,59 +1,127 @@
 import React, { useState } from "react";
 import "./index.css";
 const Home = () => {
-  const [formType, setFormType] = useState("Physical");
-  const [fullName, setFullName] = useState("");
-  const [message, setMessage] = useState("");
-  const [messageType, setMessageType] = useState("");
-  const SignUp = () => {
-    if (fullName === "") {
-      setMessage("full name");
-      setMessageType("error");
-    } else {
-      setMessage("full name .......");
-      setMessageType("success");
-      const userData = {
-        fullName:fullName,
-        formType:formType
-      }
-      console.log("userData",userData);
-      setFullName("")
-    }
-  };
+  const [selectedButton, setSelectedButton] = useState("Vefify");
+  const [selectedButton2, setSelectedButton2] = useState("Login");
+  const List = [
+    "Login",
+    "Sign Up",
+    "Update",
+    "Edit",
+    "Delete",
+    "Log Out",
+    "Vefify",
+  ];
+
+  const List2 = [
+    {
+      title: "Login",
+    },
+    {
+      title: "Sign Up",
+    },
+    {
+      title: "Update",
+    },
+    {
+      title: "Edit",
+    },
+    {
+      title: "Delete",
+    },
+    {
+      title: "Log Out",
+    },
+    {
+      title: "Vefify",
+    },
+  ];
+  const [List3, setList3] = useState([
+    {
+      title: "Login",
+      isSelected: false,
+    },
+    {
+      title: "Sign Up",
+      isSelected: false,
+    },
+    {
+      title: "Update",
+      isSelected: false,
+    },
+    {
+      title: "Edit",
+      isSelected: false,
+    },
+    {
+      title: "Delete",
+      isSelected: true,
+    },
+    {
+      title: "Log Out",
+      isSelected: false,
+    },
+    {
+      title: "Vefify",
+      isSelected: false,
+    },
+  ]);
   return (
     <div>
-      <button
-        className={formType === "Physical" ? "selected-button" : "button"}
-        onClick={() => setFormType("Physical")}
-      >
-        Physical
-      </button>
-      <button
-        className={formType === "Online" ? "selected-button" : "button"}
-        onClick={() => setFormType("Online")}
-      >
-        Online
-      </button>
-      <p>Apply form here for {formType} classes.</p>
-      <input
-        type="text"
-        placeholder="full name"
-        value={fullName}
-        onChange={(fullName) => setFullName(fullName.target.value)}
-      />
-      <p style={{ color: messageType === "error" ? "red" : "green" }}>
-        {message}
-      </p>
-      <button onClick={SignUp}>submit</button>
+      <h1>Home Page</h1>
+
+      {List.map((v, i) => {
+        return (
+          <button
+            key={i}
+            className={selectedButton === v ? "selected-button" : "button"}
+            onClick={() => setSelectedButton(v)}
+          >
+            {v}
+          </button>
+        );
+      })}
+
+      <hr />
+      {List2.map((v, i) => {
+        return (
+          <button
+            key={i}
+            className={
+              selectedButton2 === v.title ? "selected-button" : "button"
+            }
+            onClick={() => setSelectedButton2(v.title)}
+          >
+            {v.title}
+          </button>
+        );
+      })}
+
+      <hr />
+      {List3.map((v, i) => {
+        return (
+          <button
+            key={i}
+            className={v.isSelected ? "selected-button" : "button"}
+            onClick={() => {
+              // v.isSelected !== v.isSelected;
+              // setList3(v.isSelected);
+              List3[i].isSelected = !List3[i].isSelected;
+              setList3([...List3]);
+            }}
+          >
+            {v.isSelected && (
+              <img
+                src="https://www.freeiconspng.com/thumbs/check-tick-icon/tick-icon-30.png"
+                height={10}
+              />
+            )}
+            {v.title}
+          </button>
+        );
+      })}
     </div>
   );
 };
 export default Home;
-
-
-
-
-
-
-
-// Login, Sign Up ,Update, Edit, Delete ,Log Out ,Vefify 
+// Login, Sign Up ,Update, Edit, Delete ,Log Out ,Vefify
