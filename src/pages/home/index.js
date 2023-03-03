039793ed7614309b22b662faecd500d42d471daa
hishmatrai <hishmatrai.2018@gmail.com>
2023-03-03 19:47:30 +0500

    update

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,11 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import { SearchAppBar } from "../../components";
+import { useNavigate } from "react-router-dom";
+import moment from "moment";
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -11,31 +13,53 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: "center",
   color: theme.palette.text.secondary,
 }));
-
 export default function BasicGrid() {
+  const navigate = useNavigate();
+  const [usersList, setUsersList] = useState([]);
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    fetch(
+      "https://newsapi.org/v2/everything?q=apple&from=2023-03-02&to=2023-03-02&sortBy=popularity&apiKey=cdcb29843045426cb8a49ef77470cb7e"
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setUsersList(json.articles);
+        setLoader(false);
+      });
+  }, []);
+console.log(usersList)
   return (
     <div>
       <SearchAppBar />
-      <Grid container spacing={2}>
-        <Grid item xl={2} lg={3} md={4} sm={6} xs={12} >
-          <Item>xs=8</Item>
+      {loader ? (
+        <h1>Loading ....</h1>
+      ) : (
+        <Grid container spacing={2}>
+          {usersList.length === 0 ? (
+            <h1>No Data available .... </h1>
+          ) : (
+            usersList.map((v, i) => {
+              return (
+                <Grid item xl={3} lg={3} md={4} sm={6} xs={12} key={i}>
+                  <Item>
+                    <h1>
+                      {v.title.slice(0, 30)} {v.title.length > 30 && "..."}
+                    </h1>
+                    <p>{v.description}</p>
+                    <p>{moment(v.publishedAt).format("MMMM DD, YYYY")}</p>
+                    <img src={v.urlToImage} style={{ width: "100%" }} />
+                    <button
+                      onClick={() => navigate(`/blog-details-2/${v.title}`)}
+                    >
+                      View
+                    </button>
+                  </Item>
+                </Grid>
+              );
+            })
+          )}
         </Grid>
-        <Grid item xl={2} lg={3} md={4} sm={6} xs={12}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xl={2} lg={3} md={4} sm={6} xs={12}>
-          <Item>xs=4</Item>
-        </Grid>
-        <Grid item xl={2} lg={3} md={4} sm={6} xs={12}>
-          <Item>xs=8</Item>
-        </Grid>
-        <Grid item xl={2} lg={3} md={4} sm={6} xs={12}>
-          <Item>xs=8</Item>
-        </Grid>
-        <Grid item xl={2} lg={3} md={4} sm={6} xs={12}>
-          <Item>xs=8</Item>
-        </Grid>
-      </Grid>
+      )}
     </div>
   );
 }
